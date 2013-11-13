db366d65cdc9ecc3524d5e72c6e44bb3a07e65b3
Sonia Lin <sonia_lin@hotmail.com>
2013-11-13 11:39:37 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Restaurant::Application.config.secret_key_base = 'a97bcfcedf3d34bf352631b08e5d31e575b9ca9d35f374ed575bd00f4a56f084073bc30163bf1900ced4d1d4cf1508e81ac763970b94ba8644777f9f122e790e'

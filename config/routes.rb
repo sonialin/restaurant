b693f567851b11e5125596374fcab8a6ba6b37fc
Sonia Lin <sonia_lin@hotmail.com>
2013-11-13 16:49:20 +0800

    Fix icons

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Restaurant::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

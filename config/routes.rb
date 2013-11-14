471d6b66cfd3d1f445d925875efe421fdff0ad7a
Sonia Lin <sonia_lin@hotmail.com>
2013-11-14 21:15:10 +0800

    Change all positioning to relative

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Restaurant::Application.routes.draw do
-  get "pages/home"
-  get "pages/about"
-  get "pages/menu"
-  get "pages/location"
-  get "pages/contact"
+  get "home" => 'pages#home'
+  get "about" => 'pages#about'
+  get "menu" => 'pages#menu'
+  get "location" => 'pages#location'
+  get "contact" => 'pages#contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

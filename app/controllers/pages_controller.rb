7d51e27a775ad72cb10b8492bf9c90f8353a7942
Sonia Lin <sonia_lin@hotmail.com>
2014-02-26 14:48:58 +0800

    Correct en locale file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,7 @@ class PagesController < ApplicationController
   skip_before_filter :authorize
 
   def home
-    # if params[:set_locale]
-    #   redirect_to root_path(:locale => params[:set_locale])
-    # end
+
   end
 
   def about

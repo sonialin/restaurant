6a80677c5fed5d84c36f428aeafbcd1ea20713f4
Sonia Lin <sonia_lin@hotmail.com>
2014-02-26 11:58:36 +0800

    Remove translation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :authorize
-  before_filter :set_i18n_locale_from_params
+  # before_filter :set_i18n_locale_from_params
 
   protected
 
@@ -13,19 +13,19 @@ class ApplicationController < ActionController::Base
   	end
   end
   
-  def set_i18n_locale_from_params
-    if params[:locale]
-      if I18n.available_locales.include?(params[:locale].to_sym)
-        I18n.locale = params[:locale]
-      else
-        flash.now[:notice] = 
-          "#{params[:locale]} translation not available"
-        logger.error flash.now[:notice]
-      end
-    end
-  end
+  # def set_i18n_locale_from_params
+  #   if params[:locale]
+  #     if I18n.available_locales.include?(params[:locale].to_sym)
+  #       I18n.locale = params[:locale]
+  #     else
+  #       flash.now[:notice] = 
+  #         "#{params[:locale]} translation not available"
+  #       logger.error flash.now[:notice]
+  #     end
+  #   end
+  # end
 
-  def default_url_options
-    { :locale => I18n.locale }
-  end
+  # def default_url_options
+  #   { :locale => I18n.locale }
+  # end
 end

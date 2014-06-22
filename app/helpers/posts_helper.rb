1a3f897097d133a3d61409c2ed9067563e318662
Sonia Lin <sonia_lin@hotmail.com>
2014-06-22 18:44:16 +0800

    Add blog to the menu and fix layout

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,15 @@
 module PostsHelper
+	def reformatted_post_datetime(time_created_at)
+	  distance = Time.now - time_created_at
+
+	  format_for_old_posts = time_created_at.strftime("%m-%d-%Y")
+
+	  format_for_fresh_posts = distance_of_time_in_words_to_now(time_created_at) + " ago"
+	   
+	  if(distance > 10.days)
+	    format_for_old_posts
+	  else
+	    format_for_fresh_posts
+	  end
+	end
 end
